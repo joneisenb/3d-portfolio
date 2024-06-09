318ea4031c4e8125b84a73efae11b24f58fa34bf
joneisenb <j.eisenhour@outlook.com>
2024-06-09 13:12:32 -0600

    Initial upload

---
+++ src/components/Resume/resume.tsx
@@ -0,0 +1,4 @@
+
+<object data="Jon Eisenhour Resume.pdf" height="90%">
+    <p>Unable to display PDF file. <a href="C:\Users\thermaltake\Desktop\3dportfolio\testwebsite\Jon Eisenhour Resume.pdf">Download</a> instead.</p>
+</object>
