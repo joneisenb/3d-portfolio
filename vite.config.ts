2aae3a2f6d8b1c2c1bbd13ecd3b057a7e5aa1a59
joneisenb <j.eisenhour@outlook.com>
2024-06-09 15:26:55 -0600

    update vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+    plugins: [react()],
+    base: "https://joneisen.github.io/3d-portfolio/",
+  });
