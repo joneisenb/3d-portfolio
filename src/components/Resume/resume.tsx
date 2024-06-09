e7a88d7a45b5de8df121636794b041d3ef007616
joneisenb <j.eisenhour@outlook.com>
2024-06-09 15:02:28 -0600

    fixing other

--- src/components/Resume/resume.tsx
+++
@@ -1,4 +0,0 @@
-
-<object data="Jon Eisenhour Resume.pdf" height="90%">
-    <p>Unable to display PDF file. <a href="C:\Users\thermaltake\Desktop\3dportfolio\testwebsite\Jon Eisenhour Resume.pdf">Download</a> instead.</p>
-</object>
